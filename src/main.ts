d232d1d8da3ce13e87068c7ce7b1f8b1a061d21c
mark <markpackham1@gmail.com>
2022-12-20 14:55:46 +0000

    demo use of .safeParse
    https://odysee.com/@WebDevSimplified:8/learn-zod-in-30-minutes:a?r=7FU2nojSFqrsu4QQATzPqtwMSeVpNiGR&t=311

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 // Whilst Zod can be used with JS it's really aimed for use with TypeScript
-// Zod is a validation library with helpful errors
+// Zod is a validation library with helpful errors & things like .infer
 import {z} from "zod";
 
 const UserSchema = z.object({
@@ -11,3 +11,5 @@ type User = z.infer<typeof UserSchema>;
 const user = {username: "123"}
 
 console.log(UserSchema.parse(user))
+// safeParse gives a true or false response if succesful, handy for form validation
+console.log(UserSchema.safeParse(user))
