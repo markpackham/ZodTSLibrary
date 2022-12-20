a73575cb38685d27d7c64244902b90b4358864c8
mark <markpackham1@gmail.com>
2022-12-20 15:10:44 +0000

    demo remaining common types
    https://odysee.com/@WebDevSimplified:8/learn-zod-in-30-minutes:a?r=7FU2nojSFqrsu4QQATzPqtwMSeVpNiGR&t=435

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,30 @@
 // Zod is a validation library with helpful errors & things like .infer
 import {z} from "zod";
 
+// the default for types is "required" hence the use of "optional"
 const UserSchema = z.object({
   username: z.string(),
   age: z.number(),
   age2: z.bigint(),
+  age3: z.bigint(),
   birthday: z.date().optional(),
   isProgrammer: z.boolean(),
+  // null is a primitive value
+  test: z.null().optional(),
+  // undefined is a global property & default for types or a primitive value
+  test2: z.undefined().optional(),
+  // void is an operator
+  test3: z.void().optional(),
+  test4: z.any().optional(),
+  // same as "any"
+  test5: z.unknown().optional(),
+  // must never be defined
+  test6: z.never().optional(),
 })
 
 type User = z.infer<typeof UserSchema>;
 
-const user = {username: "123",age:1,age2:9007199254740991n,isProgrammer:true}
+const user = {username: "123",age:1,age2:9007199254740991n,age3:12n,isProgrammer:true}
 
 console.log(UserSchema.parse(user))
 // safeParse gives a true or false response if succesful, handy for form validation
